be6894125526c2039b2515cbb31f9f8ac0e8a4e0
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2021-06-11 14:31:33 +0900

    first commit

---
+++ section2/openfile.py
@@ -0,0 +1,8 @@
+import wave as wave
+import pyroomacoustics as pa
+import numpy as np
+
+pa.datasets.CMUArcticCorpus(basedir="../CMU_ARCTIC", download=True, speaker=["aew", "axb"])
+sample_wave_file="../CMU_ARCTIC/cmu_us_aew_arctic/wav/arctic_a0001.wav"
+
+wav = wave.open(sample_wave_file)
